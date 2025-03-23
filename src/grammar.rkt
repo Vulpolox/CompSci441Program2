17242b4a346aceb2bc94dde4a0e862b4d4cf346a
Vulpolox <jack.w.a.keys@gmail.com>
2025-03-23 16:13:53 -0500

    Restructured the grammar for compatibility w/ Brag's lexer module

--- src/grammar.rkt
+++ src/grammar.rkt
@@ -9,29 +9,25 @@
 ;; object: "world" | WORLD
 
 
-;; NOTES: terminals are string literals; non-terminals are lowercase identifiers;
+;; NOTES: terminals are all caps identifiers
+;; non-terminals are lowercase identifiers;
 ;; epsilons are ∅
 
 
 ;; GRAMMAR
 
-program: "{" stmt-list "}" "$$"
+program: LBRAC stmt-list RBRAC END
 stmt-list: stmt stmt-list | ∅
 stmt: id "=" expr ";"
-    | "if" "(" expr ")" stmt-list "endif" ";"
-    | "read" id ";"
-    | "write" expr ";"
+    | IF LPAREN expr RPAREN stmt-list ENDIF SEMICOLON
+    | READ id SEMICOLON
+    | WRITE expr SEMICOLON
 expr: id etail
     | num etail
-etail: "+" expr
-     | "-" expr
-     | compare expr
+etail: PLUS expr
+     | MINUS expr
+     | COMPARE expr
      | ∅
-id: letter+
-letter: "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z"
-      | "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z"
-num: numsign digit+
-digit: "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
-numsign: "+" | "-" | ∅
-compare: "<" | "<=" | ">" | ">=" | "==" | "!="
+id: LETTER+
+num: NUMSIGN DIGIT+
 
--- src/tokenizer.rkt
+++ src/tokenizer.rkt
@@ -1,1 +1,32 @@
 #lang racket
+
+(require br-parser-tools/lex)
+(require brag/support)
+
+;; EXAMPLE FROM DOCUMENTATION
+
+#|
+(define (tokenize ip)
+  (port-count-lines! ip)
+  (define my-lexer
+    (lexer-src-pos
+     [(repetition 1 +inf.0 numeric)
+      (token 'INTEGER (string->number lexeme))]
+     [upper-case
+      (token 'STRING lexeme)]
+     ["b"
+      (token 'STRING " ")]
+     [";"
+      (token ";" lexeme)]
+     [whitespace
+      (token 'WHITESPACE lexeme #:skip? #t)]
+     [(eof)
+      (void)]))
+  (define (next-token) (my-lexer ip))
+  next-token)
+|#
+
+
+
+
+
